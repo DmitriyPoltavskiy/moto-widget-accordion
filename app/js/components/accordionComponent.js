55363bf0759da531b25562d5ba1d8a611d8d503a
Dmitriy Poltavskiy <di77929@gmail.com>
2017-01-28 19:57:31 +0200

    Finished new logic to widget

--- app/js/components/accordionComponent.js
+++ app/js/components/accordionComponent.js
@@ -1,27 +1,18 @@
 angular.module('moto-accordionApp')
 .component('motoAccordion', {
-	transclude: true,
-	template:
-
-	'<button ng-show="$ctrl.closeAllButton">Close all</button>' +
-	'<div ng-transclude></div>',
-
 	bindings: {
-		openOnlyOneCheckbox: '=openOnlyOne',
-		closeAllButton: '=closeAll'
+		openOnlyOneCheckbox: '=openOnlyOne'
 	},
 	controller: [
 		'$element',
 		'$attrs',
 		'$scope',
-		'openAccordionService',
-		function($element, $attrs, $scope, openAccordionService) {
-
-			var vm = this;
-
-			openAccordionService.openOnlyOne = vm.openOnlyOneCheckbox;
-			// console.log('ctrl ', openAccordionService.openOnlyOne);
-			// console.log('element ', $element);
+		function($element, $attrs, $scope) {
+			this.$onInit = function () {
+				this.close = function () {
+					$element.children().removeClass('isOpen');
+				}
+			}
 		}
 	]
 });
--- app/js/components/accordionItemComponent.js
+++ app/js/components/accordionItemComponent.js
@@ -5,50 +5,30 @@ angular.module('moto-accordionApp')
 		'body': 'div'
 	},
 	template:
-
 	'<div class="panel-heading header" ng-transclude="header" ng-click="$ctrl.open()"></div>' +
 	'<section ng-transclude="body" class="content"></section>',
-
+	require: {
+		motoAccordion: '^motoAccordion'
+	},
 	controller: [
 		'$element',
 		'$attrs',
 		'$scope',
 		'openAccordionService',
 		function($element, $attrs, $scope, openAccordionService) {
-			var vm = this;
-			vm.isOpen = false;
-
-			// vm.open = function() {
-			// 	$element.toggleClass('isOpen');
-			// }
-
-			// vm.open = function() {
-			// 	if(!vm.isOpen) {
-			// 		$element.addClass('isOpen');
-			// 		vm.isOpen = true;
-			// 	}
-			// 	else {
-			// 		$element.removeClass('isOpen');
-			// 		vm.isOpen = false;
-			// 	}
-			// }
-			console.log('element ', $element);
-			
-
-			vm.open = function() {
-				if(!vm.isOpen) {
-					$element.addClass('isOpen');
-					vm.isOpen = true;
-				}
-				else {
-					$element.removeClass('isOpen');
-					vm.isOpen = false;
+			this.$onInit = function () {
+				this.open = function() {
+					if(!$element.hasClass('isOpen')) {
+						if(this.motoAccordion.openOnlyOneCheckbox === true) {
+							this.motoAccordion.close();
+						}
+						$element.addClass('isOpen');
+					}
+					else {
+						$element.removeClass('isOpen');
+					}
 				}
 			}
-
-
-			
-
 		}
 	]
 });
